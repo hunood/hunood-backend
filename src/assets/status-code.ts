c9803f6c083508f428b468a8cc8bb41e5efdbc16
Dannyel Kayke <dannyel_kayke@hotmail.com>
2021-02-11 22:29:24 -0300

    Refatoração status-code

--- src/assets/status-code.ts
+++ src/assets/status-code.ts
@@ -1,15 +1,18 @@
-import {Request, Response, NextFunction} from 'express'
+import { Request, Response, NextFunction } from 'express'
 import { StatusCodes, getReasonPhrase } from 'http-status-codes';
 import { BaseRoute } from '../typing/enums';
 import { Error } from '../typing/interfaces';
 import { t } from '../i18n';
 
-const getStatus = (code: typeof StatusCodes | number, error?: Error) => {
-    return {
-        error: error.code,
-        message: error.message,
-        code: `${code} - ${getReasonPhrase(code as unknown as number)}`,
-    };
+const getResponse = (statusCode: typeof StatusCodes | number, data: any, error?: Error) => {
+    if (error) {
+        return error = {
+            message: error?.code ? `${error.code} ${error.message}` : undefined,
+            code: `${statusCode} - ${getReasonPhrase(statusCode as unknown as number)}`,
+        }
+    }
+
+    return data;
 };
 
 const error = (code: string, message: string): Error => {
@@ -23,17 +26,17 @@ const status = (_: Request, res: Response, next: NextFunction) => {
             delete obj?.dataValues?.senha
         }
 
-        const keys = Object.keys(obj);
+        const keysOfObj = Object.keys(obj);
 
         if (res.statusCode >= 400) {
-            if (keys.length === 2 && keys[0] === 'code' && keys[1] === 'message') {
-                json.call(this, { status: getStatus(res.statusCode, obj) });
+            if (keysOfObj.length >= 2 && keysOfObj[0] === 'code' && keysOfObj[1] === 'message') {
+                json.call(this, getResponse(res.statusCode, undefined, obj));
             }
-
-            json.call(this, { error: t('errors:nao-tratado'), route: res.req.originalUrl });
-            
+            else {
+                json.call(this, { error: t('errors:nao-tratado'), route: res.req.originalUrl });
+            }
         } else {
-            json.call(this, Object.assign({ data: obj }, { status: getStatus(res.statusCode, obj) }));
+            json.call(this, getResponse(res.statusCode, obj));
         }
     };
     next();
