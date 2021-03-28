95c7560cd331919f5eba67bda670ceefc8541868
Dannyel Kayke <dannyel_kayke@hotmail.com>
2021-03-28 20:49:31 -0300

    safe token

--- src/assets/token-authentication/middlewares-autenticacao.ts
+++ src/assets/token-authentication/middlewares-autenticacao.ts
@@ -13,7 +13,7 @@ const getUsuario = async (id: any, res: Response) => {
   const autenticacao = await Autenticacao.findByPk(id);
 
   if (!autenticacao) {
-    return res.status(StatusCodes.UNPROCESSABLE_ENTITY).json(error('MTOK1001', t('codes:ONBO1001')));
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY).json(error('MTOK1001', t('codes:MTOK1001')));
   }
 
   return autenticacao;
@@ -21,14 +21,29 @@ const getUsuario = async (id: any, res: Response) => {
 
 export default {
   async bearer(req: Request, res: Response, next: NextFunction) {
-    const accessToken = req.header('Authorization').split('Bearer')[1].trim();
-    const id = await tokens.access.verifica(accessToken);
-    (req as any).auth = await getUsuario(id, res);
-    next();
+    try {
+      const accessToken = req.header('Authorization')?.split('Bearer')[1]?.trim();
+
+      if (!accessToken) {
+        return res.status(StatusCodes.IM_A_TEAPOT).json(error('MTOK2001', t('codes:MTOK2001')));
+      }
+
+      const id = await tokens.access.verifica(accessToken);
+      (req as any).auth = await getUsuario(id, res);
+      next();
+    }
+    catch (error) {
+
+    }
   },
 
   async refresh(req: Request, res: Response, next: NextFunction) {
     const refreshToken = req.header('Refresh-Authorization').split('Basic')[1].trim();
+
+    if (!refreshToken) {
+      return res.status(StatusCodes.IM_A_TEAPOT).json(error('MTOK3001', t('codes:MTOK3001')));
+    }
+
     const id = await tokens.refresh.verifica(refreshToken);
     (req as any).auth = await getUsuario(id, res);
     await tokens.refresh.invalida(refreshToken);
