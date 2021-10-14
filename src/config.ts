1c5fcedf83415deec32d31ddce585be416afb6f8
Dannyel Kayke <dannyel_kayke@hotmail.com>
2021-10-14 20:02:39 -0300

    Melhoria em variaveis de ambiente

--- src/config.ts
+++ src/config.ts
@@ -2,27 +2,21 @@ import { Config } from "./typing/interfaces";
 import { Languages } from "./typing/enums";
 import dbconfig from "./config.database";
 
-const environment = process.env.ENVIRONMENT === 'production' ? 'production' : 'development';
-const REDIS_URL = process.env.REDISCLOUD_URL.split(':');
+const environment = process.env.ENVIRONMENT === "production" ? "production" : "development";
+const REDIS_URL = process.env.REDISCLOUD_URL.split(":");
 
 export const config: Config = {
     environment,
     language: Languages.portuguese_br,
-    appName: 'Hunood Backend',
-    baseUrl: process.env.BASEURL || 'http://localhost:3001/',
+    appName: "Hunood Backend",
+    baseUrl: process.env.BASEURL || "http://localhost:3001/",
     port: Number(process.env.PORT) || 3001,
     database: dbconfig,
-    redis: environment === 'development'
-        ? {
-            password: '',
-            host: '127.0.0.1',
-            port: 6379,
-        }
-        : {
-            password: REDIS_URL[2].split('@')[0],
-            host: REDIS_URL[2].split('@')[1],
-            port: Number(REDIS_URL[3].split('/')[0]),
-        },
+    redis: {
+        password: REDIS_URL[2].split("@")[0],
+        host: REDIS_URL[2].split("@")[1],
+        port: Number(REDIS_URL[3].split("/")[0]),
+    },
     auth: {
         expiresIn: Number(process.env.JWT_EXPIRES_IN),
         salt: Number(process.env.JWT_SALT),
@@ -31,7 +25,7 @@ export const config: Config = {
     email: {
         user: process.env.EMAIL_USER,
         password: process.env.EMAIL_PASSWORD,
-        service: process.env.EMAIL_SERVICE || '',
+        service: process.env.EMAIL_SERVICE || "",
         host: process.env.EMAIL_HOST,
         port: Number(process.env.EMAIL_PORT),
         secure: false
