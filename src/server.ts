a0ab93c1db3f9082dbd37cdb57958e115179d39e
Dannyel Kayke <dannyel_kayke@hotmail.com>
2022-06-30 11:32:52 -0300

    ajuste de cors

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import { config } from './config';
 import { router } from './routes';
-import { validarInicializacoes } from './assets/validador-inicializacao'; 
+import { validarInicializacoes } from './assets/validador-inicializacao';
 import { Util } from './assets/utils';
 
 const app = express();
@@ -15,15 +15,18 @@ app.use(cors({
     origin: "https://hunood-web-psi.vercel.app"
 }));
 
-app.options('*', cors());
-
 app.use(helmet());
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://hunood-web-psi.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT')
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
     req.body = Util.recursiveObjectTo(req.body, Util.camelToSnakeCase);
     next()
 })
